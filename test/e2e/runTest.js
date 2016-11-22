18282f58a1c0a8093ae20f610e005fd650f70bbb
Faradey27 <andrii.tiertyshnyi@gmail.com>
2016-11-23 01:13:06 +0200

    Several fixes for succesful windows build

--- test/e2e/runTest.js
+++ test/e2e/runTest.js
@@ -1,10 +1,12 @@
 /* eslint-disable dot-location */
 /* eslint-disable import/no-commonjs */
 /* eslint-disable strict */
+/* eslint-disable fp/no-let */
+/* eslint-disable fp/no-mutation */
 
 'use strict';
 
-let url = '';
+let url = 'http://localhost:3001';
 
 module.exports = {
   before(browser) {
